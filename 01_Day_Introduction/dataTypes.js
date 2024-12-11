83137b2584388e95460d4697fe801d6ad35251fc
jiturre <66223727+jiturre@users.noreply.github.com>
2024-12-10 21:53:09 -0300

    Day 1

---
+++ 01_Day_Introduction/dataTypes.js
@@ -0,0 +1,2 @@
+// 5. Crea el archivo datatypes.js y utilizar el operador "typeof" de JavaScript para comprobar los diferentes tipos de datos. Comprueba el tipo de datos de cada variable
+console.log(typeof nombre,typeof variableBandera,typeof indefinida,variableNula)
---
+++ 01_Day_Introduction/variable2.js
@@ -0,0 +1,31 @@
+// 4. Crea un archivo variable.js y declarar variables y asignar tipos de datos string, booleanos, indefinidos y nulos
+
+let nombre = String;
+let variableBandera = Boolean;
+let indefinida;
+let variableNula = null;
+
+// 6. Declara cuatro variables sin asignar valores
+
+let variableSinAsignar1,
+  variableSinAsignar2,
+  variableSinAsignar3,
+  variableSinAsignar4;
+
+// 7. Declara cuatro variables con valores asignados
+
+const variableAsignada1 = 1,
+  variableAsignada2 = '2',
+  variableAsignada3 = 3.14,
+  variableAsignada4 = false;
+
+// 8. Declara variables para almacenar su nombre, apellido, estado civil, país y edad en múltiples líneas
+
+const miNombre = "Joaquin",
+  miApellido = "Gomez Iturre",
+  estaCasado = false,
+  pais = "Argentina",
+  edad = 27;
+
+console.log("Tengo " + edad + "años.")
+console.log("Tu tienes " + "25 años.")
